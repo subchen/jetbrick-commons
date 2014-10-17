fcc1631e50360d8023b0b3db572bd7f45527772f
Guoqiang Chen <subchen@gmail.com>
2014-10-17 03:45:09 +0000

    reimplement ListMap

--- src/main/java/jetbrick/collection/ListMap.java
+++ src/main/java/jetbrick/collection/ListMap.java
@@ -20,19 +20,20 @@
 package jetbrick.collection;
 
 import java.util.*;
+import jetbrick.util.tuple.NameValuePair;
 
 @SuppressWarnings("unchecked")
 public class ListMap<K, V> implements Map<K, V> {
-    private Object[] items;
+    private Entry<K, V>[] items;
     private int size;
 
     public ListMap() {
-        this.items = new Object[32];
+        this.items = new Entry[16];
         this.size = 0;
     }
 
     public ListMap(int initialCapacity) {
-        this.items = new Object[initialCapacity * 2];
+        this.items = new Entry[initialCapacity];
         this.size = 0;
     }
 
@@ -48,8 +49,8 @@ public class ListMap<K, V> implements Map<K, V> {
 
     @Override
     public boolean containsKey(Object key) {
-        for (int i = 0; i < size; i += 2) {
-            if (items[i].equals(key)) {
+        for (int i = 0; i < size; i++) {
+            if (items[i].getKey().equals(key)) {
                 return true;
             }
         }
@@ -58,8 +59,8 @@ public class ListMap<K, V> implements Map<K, V> {
 
     @Override
     public boolean containsValue(Object value) {
-        for (int i = 1; i < size; i += 2) {
-            if (items[i].equals(value)) {
+        for (int i = 1; i < size; i++) {
+            if (items[i].getValue().equals(value)) {
                 return true;
             }
         }
@@ -68,9 +69,10 @@ public class ListMap<K, V> implements Map<K, V> {
 
     @Override
     public V get(Object key) {
-        for (int i = 0; i < size; i += 2) {
-            if (items[i].equals(key)) {
-                return (V) items[i + 1];
+        for (int i = 0; i < size; i++) {
+            Entry<K, V> item = items[i];
+            if (item.getKey().equals(key)) {
+                return item.getValue();
             }
         }
         return null;
@@ -78,20 +80,19 @@ public class ListMap<K, V> implements Map<K, V> {
 
     @Override
     public V put(K key, V value) {
-        for (int i = 0; i < size; i += 2) {
-            if (items[i].equals(key)) {
-                Object old = items[i + 1];
-                items[i] = key;
-                items[i + 1] = value;
+        for (int i = 0; i < size; i++) {
+            Entry<K, V> item = items[i];
+            if (item.getKey().equals(key)) {
+                V old = item.getValue();
+                item.setValue(value);
                 return (V) old;
             }
         }
 
-        if (items.length - size < 2) {
-            items = Arrays.copyOf(items, items.length + 16);
+        if (items.length - size < 0) {
+            items = Arrays.copyOf(items, items.length * 2);
         }
-        items[size++] = key;
-        items[size++] = value;
+        items[size++] = new NameValuePair<K, V>(key, value);
         return null;
     }
 
@@ -112,7 +113,7 @@ public class ListMap<K, V> implements Map<K, V> {
 
     @Override
     public void putAll(Map<? extends K, ? extends V> m) {
-        for (Map.Entry<? extends K, ? extends V> entry : m.entrySet()) {
+        for (Entry<? extends K, ? extends V> entry : m.entrySet()) {
             put(entry.getKey(), entry.getValue());
         }
     }
@@ -127,24 +128,189 @@ public class ListMap<K, V> implements Map<K, V> {
 
     @Override
     public Set<K> keySet() {
-        Set<K> keys = new HashSet<K>(size);
-        for (int i = 0; i < size; i += 2) {
-            keys.add((K) items[i]);
-        }
-        return keys;
+        return new MapSet<K>() {
+            @Override
+            public boolean contains(Object key) {
+                return ListMap.this.containsKey(key);
+            }
+
+            @Override
+            public Iterator<K> iterator() {
+                return new KeyIterator();
+            }
+        };
     }
 
     @Override
     public Collection<V> values() {
-        List<V> values = new ArrayList<V>(size);
-        for (int i = 1; i < size; i += 2) {
-            values.add((V) items[i]);
-        }
-        return values;
+        return new MapSet<V>() {
+            @Override
+            public boolean contains(Object value) {
+                return ListMap.this.containsValue(value);
+            }
+
+            @Override
+            public Iterator<V> iterator() {
+                return new ValueIterator();
+            }
+        };
     }
 
     @Override
-    public Set<Map.Entry<K, V>> entrySet() {
-        throw new UnsupportedOperationException();
+    public Set<Entry<K, V>> entrySet() {
+        return new MapSet<Entry<K, V>>() {
+            @Override
+            public boolean contains(Object value) {
+                for (int i = 1; i < size; i++) {
+                    if (items[i].equals(value)) {
+                        return true;
+                    }
+                }
+                return false;
+            }
+
+            @Override
+            public Iterator<Entry<K, V>> iterator() {
+                return new EntryIterator();
+            }
+        };
+    }
+
+    @Override
+    public String toString() {
+        if (size == 0) {
+            return "{}";
+        }
+        StringBuilder sb = new StringBuilder();
+        sb.append('{');
+        for (Map.Entry<K, V> entry : items) {
+            if (sb.length() > 1) {
+                sb.append(", ");
+            }
+            Object key = entry.getKey();
+            Object value = entry.getValue();
+            sb.append(key == this ? "(this)" : key);
+            sb.append('=');
+            sb.append(value == this ? "(this)" : value);
+        }
+        sb.append('}');
+        return sb.toString();
+    }
+
+    abstract class MapIterator {
+        protected int index;
+
+        public MapIterator() {
+            this.index = 0;
+        }
+
+        public boolean hasNext() {
+            return index < size;
+        }
+
+        public Entry<K, V> nextEntry() {
+            return items[index++];
+        }
+
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
+    }
+
+    final class KeyIterator extends MapIterator implements Iterator<K> {
+        @Override
+        public K next() {
+            return nextEntry().getKey();
+        }
+    }
+
+    final class ValueIterator extends MapIterator implements Iterator<V> {
+        @Override
+        public V next() {
+            return nextEntry().getValue();
+        }
+    }
+
+    final class EntryIterator extends MapIterator implements Iterator<Entry<K, V>> {
+        @Override
+        public Entry<K, V> next() {
+            return nextEntry();
+        }
+    }
+
+    abstract class MapSet<E> implements Set<E> {
+        @Override
+        public int size() {
+            return size;
+        }
+
+        @Override
+        public boolean isEmpty() {
+            return size == 0;
+        }
+
+        @Override
+        public Object[] toArray() {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public Object[] toArray(Object[] a) {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public boolean add(Object e) {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public boolean remove(Object o) {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public boolean containsAll(Collection<?> c) {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public boolean addAll(Collection<? extends E> c) {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public boolean removeAll(Collection<?> c) {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public boolean retainAll(Collection<?> c) {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public void clear() {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public String toString() {
+            Iterator<E> it = iterator();
+            if (!it.hasNext()) {
+                return "[]";
+            }
+            StringBuilder sb = new StringBuilder();
+            sb.append('[');
+            for (;;) {
+                Object obj = it.next();
+                sb.append(obj == this ? "(this)" : obj);
+                if (!it.hasNext()) {
+                    sb.append(']');
+                    return sb.toString();
+                }
+                sb.append(", ");
+            }
+        }
     }
 }
